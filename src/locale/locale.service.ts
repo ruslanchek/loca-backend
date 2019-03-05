29445f3a832426df39c3a635e0c0ab015b85cad3
Ruslan Shashkov <rshashkov@icloud.com>
2019-03-05 09:19:03 +0200

    Update

--- src/locale/locale.service.ts
+++ src/locale/locale.service.ts
@@ -4,8 +4,6 @@ import { LocaleEntity } from './locale.entity';
 import { Repository } from 'typeorm';
 import { LocaleFixture } from './locale.fixture';
 
-const cache = new Map<number, LocaleEntity>();
-
 @Injectable()
 export class LocaleService {
   constructor(
@@ -16,35 +14,14 @@ export class LocaleService {
   }
 
   async findAll(): Promise<LocaleEntity[]> {
-    let items: LocaleEntity[] = [];
-
-    if (cache.size > 0) {
-      items = Array.from(cache.values());
-    } else {
-      items = await this.projectRepository.find();
-      items.map(item => cache.set(item.id, item));
-    }
-
-    return items;
+    return await this.projectRepository.find();
   }
 
   async findOne(id: number): Promise<LocaleEntity> {
-    let item: LocaleEntity = null;
-
-    if (cache.has(id)) {
-      return cache.get(id);
-    } else {
-      item = await this.projectRepository.findOne({
-        where: {
-          id,
-        },
-      });
-
-      if (item) {
-        cache.set(id, item);
-      }
-    }
-
-    return item;
+    return await this.projectRepository.findOne({
+      where: {
+        id,
+      },
+    });
   }
 }
