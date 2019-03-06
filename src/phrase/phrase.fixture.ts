e2bbb58df8890b0a3a9ff7ad5c882e183b99a2ff
Ruslan Shashkov <rshashkov@icloud.com>
2019-03-06 17:42:03 +0200

    Update

--- src/phrase/phrase.fixture.ts
+++ src/phrase/phrase.fixture.ts
@@ -4,14 +4,17 @@ import * as faker from 'faker';
 
 export class PhraseFixture extends Fixture<PhraseEntity> {
   public get entities() {
-    return this.times(faker.random.number({min: 15, max: 100}), i => {
+    return this.times(faker.random.number({ min: 15, max: 100 }), i => {
       const tags = this.times(faker.random.number(4), key => {
-        return faker.random.word('animals').toLowerCase();
+        return faker.random.word().toLowerCase().split(' ')[0];
       });
 
       return {
         id: faker.random.number(),
-        phraseId: faker.random.word().toUpperCase().replace('\s*', '_'),
+        phraseId: faker.random
+          .word()
+          .toUpperCase()
+          .replace(/[\W_]+/g, '_'),
         tags,
       };
     });
